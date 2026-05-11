cc5ad417707f7d7574c1f3d758be33d45b2a0a77
abitulena <lenazihalova800@mail.ru>
2026-05-11 15:28:24 +1000

    Update Film.kt

--- app/src/main/java/com/example/ghiblifilms4hw/model/Film.kt
+++ app/src/main/java/com/example/ghiblifilms4hw/model/Film.kt
@@ -3,7 +3,6 @@ package com.example.ghiblifilms4hw.model
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -15,9 +14,7 @@ data class Film(
     val description: String? = null,
     val director: String? = null,
     val producer: String? = null,
-    @SerializedName("release_date")
     val releaseDate: String? = null,
-    @SerializedName("rt_score")
     val rtScore: String? = null,
     val image: String? = null,
     val isFavorite: Boolean = false
