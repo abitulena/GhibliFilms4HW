0c0c4cbc862822d3a730587c41d89b90a697e39f
abitulena <lenazihalova800@mail.ru>
2026-05-11 15:24:56 +1000

    Update GhibliApiService.kt

--- app/src/main/java/com/example/ghiblifilms4hw/data/remote/GhibliApiService.kt
+++ app/src/main/java/com/example/ghiblifilms4hw/data/remote/GhibliApiService.kt
@@ -1,13 +1,12 @@
 package com.example.ghiblifilms4hw.data.remote
 
-import com.example.ghiblifilms4hw.model.Film
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface GhibliApiService {
     @GET("films")
-    suspend fun getFilms(): List<Film>
+    suspend fun getFilms(): List<FilmDto>
 
     @GET("films/{id}")
-    suspend fun getFilmById(@Path("id") id: String): Film
+    suspend fun getFilmById(@Path("id") id: String): FilmDto
 }
