01e6e23ffe8b4dfd54064b78d431a3fa18d4c61a
abitulena <lenazihalova800@mail.ru>
2026-05-25 11:54:03 +1000

    1

--- app/src/main/java/com/example/ghiblifilms4hw/data/local/FilmDatabase.kt
+++ app/src/main/java/com/example/ghiblifilms4hw/data/local/FilmDatabase.kt
@@ -6,7 +6,7 @@ import com.example.ghiblifilms4hw.model.Film
 
 @Database(
     entities = [Film::class],
-    version = 5,
+    version = 6,
     exportSchema = false
 )
 abstract class FilmDatabase : RoomDatabase() {
